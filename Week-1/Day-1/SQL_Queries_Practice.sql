dc87a0bd59b3e481d8125e49163bc838255c8b2c
KAVURI VEERA VENKATA JAGADEESH <145770710+Jagadeeshkavuri1@users.noreply.github.com>
2026-05-06 17:19:07 +0530

    Add basic SQL queries for Employee and Department tables

--- Week-1/Day-1/SQL_Queries_Practice.sql
+++ Week-1/Day-1/SQL_Queries_Practice.sql
@@ -1,1 +1,16 @@
-sql commands 
+-- Basic Queries
+-- Q1) Select all columns from the Employee table
+select * from Employee; 
+-- Q2) Select only the name and salary columns from the Employee table
+select name, salary from Employee; 
+-- Q3)  Select employees who are older than 30
+select * from Employee where age > 30; 
+-- Q4) Select the names of all departments
+select name from Department; 
+-- Q5) Select employees who work in the IT department.
+select * from Employee
+where department_id = (
+    select department_id 
+    from Department 
+    where name = 'IT'
+);
